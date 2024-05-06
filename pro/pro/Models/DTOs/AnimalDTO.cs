3261c8a3b0cde3f4c8d01d51c50f28abaf8c84ba
unluckyoli <s28526@pjwstk.edu.pl>
2024-05-07 00:05:35 +0200

    :)

---
+++ pro/pro/Models/DTOs/AnimalDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace pro.Models.DTOs;
+
+public class AnimalDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public DateTime AdmissionDate { get; set; }
+    public OwnerDto Owner { get; set; } = null!;
+    public List<ProcedureDto> Procedures { get; set; } = null!;
+}
